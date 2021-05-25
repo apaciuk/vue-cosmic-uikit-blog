c78890bac207324b641d94bf1ed491c78271b7ca
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-25 08:04:04 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+<body data-uk-filter="target: .js-filter; animation: fade">
+  <main>
+  <BaseHeader />
+  <Nuxt />
+ <OffCanvas />
+  <BaseFooter />
+  </main>
+</body>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+* {
+box-sizing: border-box;
+}
+</style>
+
